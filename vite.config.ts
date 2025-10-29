0d2d4ff8e1074006597a4b5bc03e52fc36698c8c
zigaygit <domanitorapp@gmail.com>
2025-10-29 19:43:28 +0000

    Update Vite config to exclude 'fsevents' in build
    Added rollupOptions to exclude 'fsevents' during build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "./",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['fsevents'], // ⬅️ Tambahkan ini!
+    },
   },
 });
